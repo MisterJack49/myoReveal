e48693a5d04f329a1c0fcf542ba4d383bfaad16a
unknown <usrlocal@Pcistiaetu179.ad.univ-angers.fr>
2014-11-14 16:49:05 +0100

    Start of initialization

--- app.js
+++ app.js
@@ -2,13 +2,37 @@ var Myo = require('myo');
 
 var myo = Myo.create();
 
+var initialized = false;
+
+var RPYInit= []
+
+
+if(!initialized && RPYInit.length<5){
+
+	console.log("Put your arm at rest position");
+	console.log("Do Fist");
+
+	myo.on('fist', function(edge){
+		if(edge){				
+			var RPY = {
+				'roll' : getRoll(myo.lastIMU.orientation),
+				'pitch': getPitch(myo.lastIMU.orientation),
+				'yaw'  : getYaw(myo.lastIMU.orientation)
+			};
+			RPYInit.push(RPY);
+			console.log(RPYInit);;	
+			console.log("Release Fist");
+		}
+	});
+}
+
 myo.lock();
 
 myo.on('lock',function(edge){
 	myo.vibrate('short');
 })
 
-myo.on('thumb_to_pinky',function(edge){
+myo.on('wave_in',function(edge){
 	myo.timer(edge, 500, function(){
 		myo.vibrate('short').vibrate('short');
 		myo.unlock(5000);
@@ -24,33 +48,11 @@ myo.on('fist', function(edge){
 	}
 });
 
-myo.on('fingers_spread', function(edge){
-	var done = false;
-	if (!myo.isLocked && !done) {
-		done=true;
-		myo.on('orientation', function(data){
-			
-			
-			var yaw = Math.atan2(2.0 * (data.w * data.z + data.x * data.y), 1.0 - 2.0 * (data.y * data.y + data.z * data.z));
-
-			
-			
-			var yaw_w = ((yaw + Math.PI/2.0)/Math.PI * 18);
-
-			//console.log('Roll : ' + roll_w);
-			//console.log('Pitch : ' + pitch_w);
-			//console.log('Yaw : ' + yaw_w);
-			//console.log(myo.lastIMU.orientation);
-			console.log(getRoll(myo.lastIMU.orientation));
-		});
-	};
-});
-
 myo.on('imu', function(data){
 	var orientation = data.orientation;
-		console.log("Roll : " + getRoll(orientation) );
-		console.log("Pitch : " + getPitch(orientation) );
-});
+		//console.log("Roll : " + getRoll(orientation) );
+		//console.log("Pitch : " + getPitch(orientation) );
+	});
 
 function getRoll(data){
 	var roll = Math.atan2(2.0 * (data.w * data.x + data.y * data.z), 1.0 - 2.0 * (data.x * data.x + data.y * data.y));
@@ -69,4 +71,3 @@ function getYaw(data){
 	var yaw_w = ((yaw + Math.PI/2.0)/Math.PI * 18);
 	return yaw_w;
 }
-
